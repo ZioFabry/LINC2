f5db63d11e787ad8f3b54038346f5ba3b7cadaa7
sherlockcoin <sherlockcoin@gmail.com>
2019-05-10 23:08:18 +0530

    Make Files Updated
    Compiling Issues Fixed

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-# Copyright (c) 2013-2016 The Bitcoin Core developers
-# Copyright (c) 2017-2018 The Proton Core developers
-# Copyright (c) 2018-2019 The Linc Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
 set -e
 srcdir="$(dirname $0)"
 cd "$srcdir"
@@ -12,6 +6,4 @@ if [ -z ${LIBTOOLIZE} ] && GLIBTOOLIZE="`which glibtoolize 2>/dev/null`"; then
   LIBTOOLIZE="${GLIBTOOLIZE}"
   export LIBTOOLIZE
 fi
-which autoreconf >/dev/null || \
-  (echo "configuration failed, please install autoconf first" && exit 1)
 autoreconf --install --force --warnings=all
